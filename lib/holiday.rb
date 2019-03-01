8718e5a082fc29117ad82ef8b056b345d683468c
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-03-01 05:32:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -16,7 +16,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].collect { |k, v| v }.flatten
+  holiday_hash[:winter].collect { |key, value| value }.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
