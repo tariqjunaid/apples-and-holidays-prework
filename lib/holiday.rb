8becf5db111d8bb23ee59c2efe43210619cb4a60
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-03-01 04:19:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_hash[:spring].each { |key, value| value << supply }
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
