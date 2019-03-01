a415f8b61ced7548ffa4230d6431206c53831405
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-03-01 04:16:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,8 +52,6 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
   holiday_hash.map do |key, value|
   value.map do |keys, values|
     if values.include?("BBQ")
